9aed4a2ad21bb16b75114a98dc45b2b9e03a89c8
ikana82 <ikanurasih82@gmail.com>
2025-06-04 14:40:12 +0700

    Update function

--- script.js
+++ script.js
@@ -11,7 +11,22 @@ const daily = {
     temperature_2m: [11.5, 11.8, 13.7, 15.5, 16.5, 15.3, 13.5],
 };
 
-console.log(daily)
+function fetchingData() {
+    const cardContainer = document.getElementById("card-container");
+
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    console.log(daily.time[0]);
+    console.log(
+        new DataTransfer(daily.time[0]).toLocaleDateString("id-ID",)
+    );
+}
+
+// console.log(daily)
 
 // function fetchingData () {
 //     const cardContainer = document.getElementById("card-container")
@@ -20,4 +35,4 @@ console.log(daily)
 //     console.log(new Date(daily.time[0]))
 // }
 
-// fetchingData();
+fetchingData();
