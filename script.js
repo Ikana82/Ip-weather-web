54d0c347ab7795ef63bf671b2b8c96c36cbd751e
ikana82 <ikanurasih82@gmail.com>
2025-06-04 14:29:39 +0700

    Review Looping

---
+++ script.js
@@ -0,0 +1,23 @@
+const daily = {
+    time: [
+        "2024-10-14",
+        "2024-10-15",
+        "2024-10-16",
+        "2024-10-17",
+        "2024-10-18",
+        "2024-10-19",
+        "2024-10-20",
+    ],
+    temperature_2m: [11.5, 11.8, 13.7, 15.5, 16.5, 15.3, 13.5],
+};
+
+console.log(daily)
+
+// function fetchingData () {
+//     const cardContainer = document.getElementById("card-container")
+
+//     console.log(daily.time[0])
+//     console.log(new Date(daily.time[0]))
+// }
+
+// fetchingData();
